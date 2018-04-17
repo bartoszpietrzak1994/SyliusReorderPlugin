1a006ef0f5d69c5d358b31ba1c14879a01ee21e8
Bartosz Pietrzak <bartosz.pietrzak1994@gmail.com>
2018-04-17 14:13:46 +0200

    Squashed commits from previous repository (moved to Sylius forked one)

--- src/SyliusCustomerReorderPlugin.php
+++ src/SyliusCustomerReorderPlugin.php
@@ -2,12 +2,12 @@
 
 declare(strict_types=1);
 
-namespace Acme\SyliusExamplePlugin;
+namespace Sylius\CustomerReorderPlugin;
 
 use Sylius\Bundle\CoreBundle\Application\SyliusPluginTrait;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-final class AcmeSyliusExamplePlugin extends Bundle
+final class SyliusCustomerReorderPlugin extends Bundle
 {
     use SyliusPluginTrait;
 }
